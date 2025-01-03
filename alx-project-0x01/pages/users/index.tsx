f048a860803f3f1725fac952009288994dfaf8b0
yuslove1 <adexyuslove@gmail.com>
2025-01-03 03:41:28 -0800

    refactor: minor change(variable name)

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -2,8 +2,8 @@ import UserCard from "@/components/common/UserCard";
 import Header from "@/components/layout/Header";
 import { UserProps } from "@/interfaces";
 
-const Users: React.FC<PostProps[]> = ({ users }) => {
-  console.log(users);
+const Users: React.FC<PostProps[]> = ({ posts }) => {
+  console.log(posts);
   return (
     <div className="flex flex-col h-screen">
       <Header />
@@ -15,7 +15,7 @@ const Users: React.FC<PostProps[]> = ({ users }) => {
           </button>
         </div>
         <div className="grid grid-cols-3 gap-2 ">
-          {users?.map(
+          {posts?.map(
             ({ name, username, email, id, address }: UserProps, key: id) => (
               <UserCard
                 name={name}
@@ -35,11 +35,11 @@ const Users: React.FC<PostProps[]> = ({ users }) => {
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await response.json();
+  const posts = await response.json();
 
   return {
     props: {
-      users,
+      posts,
     },
   };
 }
