202feecc32852f0672cbefb3be860d582e853086
yuslove1 <adexyuslove@gmail.com>
2025-01-03 04:04:31 -0800

    refactor: minor change

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -15,7 +15,7 @@ const Users: React.FC<PostProps[]> = ({ posts }) => {
           </button>
         </div>
         <div className="grid grid-cols-3 gap-2 ">
-          {posts?.map(
+          {posts.map(
             ({ name, username, email, id, address }: UserProps, key: id) => (
               <UserCard
                 name={name}
